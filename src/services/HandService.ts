eb2b378891fc321194f3baa53b8617a266297cb4
Félicien François <felicien@tweakstyle.com>
2016-05-26 22:28:39 +0200

    HandService implementation

--- src/services/HandService.ts
+++ src/services/HandService.ts
@@ -5,5 +5,34 @@ export interface HandService {
     match(player1: Hand, player2: Hand) : {winner: Winner, whatHappened: string};
 }
 
+class HandServiceImpl implements HandService {
+
+    match(player1: Hand, player2: Hand) : {winner: Winner, whatHappened: string} {
+        if (player1.equals(player2)) {
+            return {
+                winner : Winner.DRAW,
+                whatHappened : "Egalité"
+            };
+        }
+        if (player1.defeat[player2.id]) {
+            return {
+                winner : Winner.PLAYER1_WIN,
+                whatHappened : player1.defeat[player2.id]
+            };
+        }
+        if (player2.defeat[player1.id]) {
+            return {
+                winner : Winner.PLAYER2_WIN,
+                whatHappened : player2.defeat[player1.id]
+            };
+        }
+        return {
+            winner : Winner.UNKNOWN,
+            whatHappened : "Résultat inconnu"
+        };
+    }
+
+}
+
 // singleton
 export var handService : HandService = new HandServiceImpl();
