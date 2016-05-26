d8952563d350a4985a9753b1cd684229356541f2
Félicien François <felicien@tweakstyle.com>
2016-05-26 23:23:17 +0200

    fix encoding

--- src/models/Hand.ts
+++ src/models/Hand.ts
@@ -1,19 +1,19 @@
 export class Hand {
     static ROCK = new Hand("rock", "Pierre", {
-        "lizard" : "La Pierre écrase le Lézard",
-        "scissors" : "La Pierre émousse les Ciseaux"
+        "lizard" : "La Pierre Ã©crase le LÃ©zard",
+        "scissors" : "La Pierre Ã©mousse les Ciseaux"
     });
     static PAPER = new Hand("paper", "Feuille", {
         "rock" : "La Feuille recouvre la Pierre",
-        "spock" : "La Feuille discrédite Spock"
+        "spock" : "La Feuille discrÃ©dite Spock"
     });
     static SCISSORS = new Hand("scissors", "Ciseaux", {
-        "lizard" : "Les Ciseaux décapite le Lézard",
+        "lizard" : "Les Ciseaux dÃ©capite le LÃ©zard",
         "paper" : "Les Ciseaux coupe la Feuille"
     });
-    static LIZARD = new Hand("lizard", "Lézard", {
-        "paper" : "Le Lézard mange la Feuille",
-        "spock" : "Le Lézard empoisonne Spock"
+    static LIZARD = new Hand("lizard", "LÃ©zard", {
+        "paper" : "Le LÃ©zard mange la Feuille",
+        "spock" : "Le LÃ©zard empoisonne Spock"
     });
     static SPOCK = new Hand("spock", "Spock", {
         "rock" : "Spock vaporise la Pierre",
