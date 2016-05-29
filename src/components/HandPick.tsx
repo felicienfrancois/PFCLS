393227406b3db984b2e303d9d0b9b73f42b4c788
Félicien François <felicien@tweakstyle.com>
2016-05-29 22:12:42 +0200

    add key to li

--- src/components/HandPick.tsx
+++ src/components/HandPick.tsx
@@ -3,16 +3,15 @@ import * as React from "react";
 import {Hand} from "../models/Hand";
 
 export interface HandPickProps { hands: Hand []; onPick: (hand: Hand) => void; };
-export interface HandPickState { hand: Hand; };
 
-export class HandPick extends React.Component<HandPickProps, HandPickState> {
+export class HandPick extends React.Component<HandPickProps, {}> {
 
     render() {
         return (
             <ul className="HandPick">
             {
                 this.props.hands.map(hand => {
-                    return <li><button key={hand.id} ref={hand.id} class={hand.id} onClick={this.props.onPick.bind(null,hand)}>{hand.name}</button></li>;
+                    return <li key={"li_" + hand.id}><button key={hand.id} ref={hand.id} class={hand.id} onClick={this.props.onPick.bind(null,hand)}>{hand.name}</button></li>;
                 })
             }
             </ul>
